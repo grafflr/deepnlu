8823b604efe33f1d65848e53f7acb49ab0a71b55
Craig Trim <craig@grafflr.ai>
2022-05-25 22:02:52 -0700

    #3 Remove Find Inference

--- deepnlu/datablock/svc/__init__.py
+++ deepnlu/datablock/svc/__init__.py
@@ -4,12 +4,10 @@ from .find_wordnet import FindWordnet
 from .find_synonyms import FindSynonyms
 from .find_dictionaries import FindDictionaries
 from .find_spans import FindSpans
-from .find_inference import FindInference
 from .find_types import FindTypes
 from .find_labels import FindLabels
 from .find_requires import FindRequires
 from .find_stopwords import FindStopWords
-from .find_comments import FindComments
 from .find_implies import FindImplies
 from .find_uses import FindUses
 from .find_reviews import FindReviews
@@ -18,7 +16,5 @@ from .find_similar import FindSimilar
 from .find_recommendations import FindRecommendations
 from .find_roles import FindRoles
 from .find_questions import FindQuestions
-from .find_speech_disorder import FindSpeechDisorder
-from .find_praxis_pathology import FindPraxisPathology
 from .find_pronouns import FindPronouns
 from .find_slack_ids import FindSlackIDs
