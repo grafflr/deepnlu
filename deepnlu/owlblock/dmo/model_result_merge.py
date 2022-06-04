d9431a6d1c9f7ddc6200ca6fe3b4e6c70134ed7a
Craig Trim <craig.trim@causalitylink.com>
2022-06-03 21:57:20 -0700

    Fix Merge Defect

--- deepnlu/owlblock/dmo/model_result_merge.py
+++ deepnlu/owlblock/dmo/model_result_merge.py
@@ -3,6 +3,8 @@
 """ Merge Multiple Model results into a single and cohesive structure """
 
 
+from collections import defaultdict
+
 from baseblock import BaseObject
 
 from askowl.dto import QueryResultType
@@ -26,7 +28,7 @@ class ModelResultMerge(BaseObject):
                result_type: QueryResultType) -> list:
 
         if result_type == QueryResultType.DICT_OF_STR2LIST:
-            d_merge = {}
+            d_merge = defaultdict(list)
             for d_result in results:
                 for k in d_result:
                     if k not in d_merge:
@@ -35,16 +37,17 @@ class ModelResultMerge(BaseObject):
                     for value in d_result[k]:
                         if value not in d_merge[k]:
                             d_merge[k].append(value)
-            return d_merge
+            
+            return dict(d_merge)
 
         elif result_type == QueryResultType.DICT_OF_STR2DICT:
-            d_merge = {}
+            d_merge = defaultdict(list)
             for d_result in results:
                 for k in d_result:
                     for value in d_result[k]:
                         d_merge[k].append(value)
 
-            return d_merge
+            return dict(d_merge)
 
         else:
             raise NotImplementedError
